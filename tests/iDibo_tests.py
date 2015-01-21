2461230e6753242d227ce9ee51abc360d712a71d
Satope Oluyemisi <satopeoladayo@gmail.com>
2015-01-21 11:23:25 +0100

    an error with the last commit was corrected

--- tests/iDibo_tests.py
+++ tests/iDibo_tests.py
@@ -1,26 +1,33 @@
 import unittest
+from gluon.shell import exec_environment
+from gluon.contrib.test_helpers import form_postvars
+
 from gluon.globals import Request
+
 
 db = test_db
 
-execfile("applications/iDibo/controllers/default.py", globals())
+#execfile("applications/iDibo/controllers/default.py", globals())
 db(db.auth_user.id > 0).delete()
 db.commit()
 
 
 class TestRegister(unittest.TestCase):
-    def setup(self):
-        request = Request()
-
+    def setUp(self):
+        #request = Request()
+        self.controller = exec_environment('applications/iDibo/controllers/default.py')
 
     def test_register_title(self):
-        request.vars['email'] = 'lala@gmail.com'
-        resp = register()
+        form_postvars('auth_user', {'email': 'pp@gmail.com', 'first_name': 'Ana'}, request, action='create',
+                      record_id=None)
+        resp = self.controller.register()
+
+
         for key in resp.iteritems():
             print key
-        self.assertEquals(1, len(resp["auth_user"]))
-        #my_title = self.request.register.response.title
-        #self.assertEqual(my_title, 'iDibo Registration')
+        self.assertEquals('Ana', resp['first_name'])
+
+
 
 
 suite = unittest.TestSuite()
